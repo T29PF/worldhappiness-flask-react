f773ad6da2a4ad9a46a9a7b15c8a477ef9ad4cb0
Arvind Rawat <priya.bhandari@gmail.com>
2018-02-03 23:03:16 -0500

    First Commit

--- microservices/ui/app/src/App.js
+++ microservices/ui/app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">World Happiness Report</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Choose one of the following metric
         </p>
       </div>
     );
